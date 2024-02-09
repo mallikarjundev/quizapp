56313eea6c3997ece764d5b0854dcc9c04af2460
arjunoggu <mallikarjundev2@gmail.com>
2024-02-09 15:47:48 -0500

    created service layer

--- src/main/java/com/example/quizapp/controller/QuestionController.java
+++ src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -1,5 +1,7 @@
-package com.example.quizapp;
+package com.example.quizapp.controller;
 
+import com.example.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("question")
 public class QuestionController {
+    @Autowired
+    QuestionService questionService;
     @GetMapping("allQuestions")
     public String getAllQuestions(){
-        return "Hi, These are your questions";
+        return questionService.getAllQuestions();
     }
 }
---
+++ src/main/java/com/example/quizapp/service/QuestionService.java
@@ -0,0 +1,7 @@
+package com.example.quizapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuestionService {
+}
