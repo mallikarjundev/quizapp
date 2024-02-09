3b3b19b2e161cacde3be18e54ec09af2e5b1aac0
arjunoggu <mallikarjundev2@gmail.com>
2024-02-09 16:01:37 -0500

    created method for allquestions

--- src/main/java/com/example/quizapp/controller/QuestionController.java
+++ src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -1,10 +1,13 @@
 package com.example.quizapp.controller;
 
+import com.example.quizapp.Question;
 import com.example.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("question")
@@ -12,7 +15,7 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("allQuestions")
-    public String getAllQuestions(){
+    public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 }
--- src/main/java/com/example/quizapp/service/QuestionService.java
+++ src/main/java/com/example/quizapp/service/QuestionService.java
@@ -1,7 +1,13 @@
 package com.example.quizapp.service;
 
+import com.example.quizapp.Question;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
+    public List<Question> getAllQuestions() {
+
+    }
 }
